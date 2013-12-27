358e676b5a53899c865c931923b55262782da2f6
Nalin.x.Linux <Nalin.x.Linux@gmail.com>
2013-12-27 03:49:30 -0500

    espeak,find,spell_check,audio_converter,go_to_page,readme,find and replace

--- lios/main.py
+++ lios/main.py
@@ -38,24 +38,17 @@ from lios.basic_editor import find
 from lios.basic_editor import find_and_replace
 
 from lios.preferences import lios_preferences
+from lios.global_var import *
 
 class linux_intelligent_ocr_solution(editor,lios_preferences):
 	def __init__ (self,filename=None):
 		self.guibuilder = Gtk.Builder()
 		self.guibuilder.add_from_file("%s/ui/ui.glade" %(global_var.data_dir))
 		self.window = self.guibuilder.get_object("window")
-		self.textbuffer = self.guibuilder.get_object("textbuffer")
 		self.textview = self.guibuilder.get_object("textview")
+		self.textbuffer = self.textview.get_buffer();
 		self.guibuilder.connect_signals(self)
 		
-		
-
-
-		#Espeak Voice List
-		self.voice_list=[]
-		for item in espeak.list_voices():
-			self.voice_list.append(item.name)
-#		espeak.set_SynthCallback(self.espeak_event)
 
 		#Reading Dictionarys		
 		self.key_value = {"eng" : "en","afr" : "af","am" : "am","ara" : "ar","ara" : "ar","bul" : "bg","ben" : "bn","br" : "br","cat" : "ca","ces" : "cs","cy" : "cy","dan" : "da","ger" : "de","ger" : "de","ell" : "el","eo" : "eo","spa" : "es","est" : "et","eu" : "eu","fa" : "fa","fin" : "fi","fo" : "fo","fra" : "fr","ga" : "ga","gl" : "gl","gu" : "gu","heb" : "he","hin" : "hi","hrv" : "hr","hsb" : "hsb","hun" : "hu","hy" : "hy","id" : "id","is" : "is","ita" : "it","kk" : "kk","kn" : "kn","ku" : "ku","lit" : "lt","lav" : "lv","mal" : "ml ","mr" : "mr ","dut" : "nl","no" : "no","nr" : "nr","ns" : "ns ","or" : "or ","pa" : "pa ","pol" : "pl ","por" : "pt","por" : "pt","por" : "pt","ron" : "ro","rus" : "ru ","slk" : "sk","slv" : "sl","ss" : "ss","st" : "st","swe" : "sv","tam" : "ta","tel" : "te","tl" : "tl","tn" : "tn","ts" : "ts","ukr" : "uk","uz" : "uz","xh" : "xh","zu" : "zu" }
@@ -63,7 +56,17 @@ class linux_intelligent_ocr_solution(editor,lios_preferences):
 		#Getting Preferences Values
 		self.read_preferences()
 		self.activate_preferences()
+		
+		
+		Gdk.threads_init()
 
+
+
+		#Espeak Voice List
+		self.voice_list=[]
+		for item in espeak.list_voices():
+			self.voice_list.append(item.name)
+		#espeak.set_SynthCallback(espeak_event)
 
 		#Preferences signals
 		General_Preferences = self.guibuilder.get_object("General_Preferences")
@@ -75,13 +78,134 @@ class linux_intelligent_ocr_solution(editor,lios_preferences):
 		Preferences_CamaraWebcam = self.guibuilder.get_object("Preferences_CamaraWebcam")
 		Preferences_CamaraWebcam.connect("activate",self.preferences,3)		
 
+		if (filename):
+			 self.textbuffer.set_text(open(filename,"r").read())
+			 self.save_file_name = filename
 		
 		self.window.maximize();
 		self.window.show()
 		Gtk.main();
-					
 		
+
+	def go_to_page(self,wedget,data=None):
+		iter,end = self.textbuffer.get_bounds()
+		adj = Gtk.Adjustment(value=1, lower=1, upper=self.starting_page_number-1, step_incr=1, page_incr=5, page_size=0) 
+		spinbutton_line = Gtk.SpinButton()
+		spinbutton_line.set_adjustment(adj)
+		spinbutton_line.set_value(0)		
+		spinbutton_line.show()
+
+		dialog =  Gtk.Dialog("Go to Page ",self.window,True,("Go", Gtk.ResponseType.ACCEPT,"Close!", Gtk.ResponseType.REJECT))
+		spinbutton_line.connect("activate",lambda x : dialog.response(Gtk.ResponseType.ACCEPT))
+		box = dialog.get_content_area();
+		box.add(spinbutton_line)
+		spinbutton_line.grab_focus()
+		dialog.show_all()
+		response = dialog.run()
+		if response == Gtk.ResponseType.ACCEPT:
+			to_go = spinbutton_line.get_value_as_int()
+			if self.page_numbering_type == 0:
+				word = "Page-{0}".format(to_go)
+			else:
+				if to_go % 2 == 0:
+					word = "Page-{0}-{1}".format(to_go-1,to_go)
+				else:
+					word = "Page-{0}-{1}".format(to_go,to_go+1)	
+			
+			results = iter.forward_search(word, 0, end)
+			if results:
+				start,end = results
+				self.textbuffer.place_cursor(start)
+				self.textview.scroll_to_iter(start, 0.0,False,0.0,0.0)
+			dialog.destroy()
+		else:
+			dialog.destroy()
+
+
+	def open_readme(self,widget,data=None):
+		with open("{0}/Data/Readme".format(data_dir)) as file:
+			self.textbuffer.set_text(file.read())
+			start = self.textbuffer.get_start_iter()
+			self.textbuffer.place_cursor(start)
+					
+	def about(self,wedget,data=None):
+		guibuilder_about = Gtk.Builder()
+		guibuilder_about.add_from_file("%s/ui/about.glade" % (global_var.data_dir))
+		window_about = guibuilder_about.get_object("aboutdialog")
+		window_about.connect("close", lambda x: print("Hello"))
+		window_about.show()
+
+	def audio_converter(self,widget):
+		try:
+			start,end = self.textbuffer.get_selection_bounds()
+		except ValueError:
+			start,end = self.textbuffer.get_bounds()
+		text = self.textbuffer.get_text(start,end,False)		
+		converter.record(text)
+	def spell_check(self,widget):
+		spell_check(self.textview,self.textbuffer,self.language,self.enchant_language)
+	def find(self,widget):
+		find(self.textview,self.textbuffer,self.language).window.show()
+	def find_and_replace(self,widget):
+		find_and_replace(self.textview,self.textbuffer,self.language).window.show()
+
+    # Read the text
+	def Read_Stop(self,wedget,data=None):
+		image_read_stop = self.guibuilder.get_object("image_read_stop")
+		if espeak.is_playing() == False:
+			image_read_stop.set_from_file("/usr/share/lios/ui/stop")
+			self.textbuffer.remove_tag(self.highlight_tag, self.textbuffer.get_start_iter(),self.textbuffer.get_end_iter())
+			mark = self.textbuffer.get_insert()
+			start = self.textbuffer.get_iter_at_mark(mark)
+			end = self.textbuffer.get_end_iter()
+			self.to_count = start.get_offset()
+			text = self.textbuffer.get_text(start,end,False)
+			espeak.set_SynthCallback(self.espeak_event)
+			espeak.synth(text)
+			self.textview.set_editable(False)
+		else:
+			espeak.cancel()
+			image_read_stop.set_from_file("/usr/share/lios/ui/play")
+			self.textbuffer.remove_tag(self.highlight_tag, self.textbuffer.get_start_iter(),self.textbuffer.get_end_iter())
+			self.textview.set_editable(True)
+			
+		
+	def espeak_event(self,event, pos, length):
+		#Gdk.threads_enter()
+		if event == espeak.core.event_WORD:
+			pos += self.to_count-1
+			s = self.textbuffer.get_iter_at_offset(pos)
+			e = self.textbuffer.get_iter_at_offset(length+pos)			
+			
+			self.textbuffer.remove_all_tags(self.textbuffer.get_start_iter(),self.textbuffer.get_end_iter())
+			self.textbuffer.apply_tag(self.highlight_tag, s, e)
+
+		if event == espeak.event_END:
+			self.point = self.textbuffer.get_iter_at_offset(pos+self.to_count)
+			self.textbuffer.place_cursor(self.point)
+			self.textview.scroll_to_iter(self.point, 0.0, use_align=True, xalign=0.0, yalign=0.2)
+
+							
+					
+		if event == espeak.event_MSG_TERMINATED:
+			espeak._playing = False
+			self.textview.set_editable(True)
+			try:
+				self.textbuffer.remove_all_tags(self.textbuffer.get_start_iter(),self.textbuffer.get_end_iter())
+			except:
+				pass
+
+		if not espeak.is_playing():
+			mark = self.textbuffer.get_insert()
+			start = self.textbuffer.get_iter_at_mark(mark)
+			end = self.textbuffer.get_end_iter()
+			self.to_count = start.get_offset()
+			text = self.textbuffer.get_text(start,end,False)
+			if (text != ""):
+				espeak.synth(text)
+
+		#Gdk.threads_leave()		
+		return True
 		
 if __name__ == "__main__":
 	linux_intelligent_ocr_solution()
-	
