839c4ee29af5a7dfc23315777a9dee1010ff481d
Nalin.x.Linux <Nalin.x.Linux@gmail.com>
2014-01-11 17:43:03 +0530

    Brightness options enabled, Scanimage Driver support added, X-Axis Problem Solved

--- lios/scanner.py
+++ lios/scanner.py
@@ -25,7 +25,9 @@ import os
 import multiprocessing
 from multiprocessing import Pipe
 class scanner():
-	def __init__(self,device,scanner_mode_switching):
+	def __init__(self,device,driver,scanner_mode_switching):
+		self.driver = driver
+		self.device = device
 		sane_version = sane.init()
 		try:
 			self.scanner = sane.open(device[0])
@@ -40,8 +42,17 @@ class scanner():
 					for mode in ['Lineart', 'Gray', 'Color']:
 						if mode in option[-1:][0]:
 							self.scanner.mode = mode
+							self.scanner_mode = mode
 							break
-			
+			else:
+				self.scanner_mode = 'Color'
+
+			#X Axis for scan Area
+			option = self.get_scanner_option('br-x')
+			if option:
+				self.scanner.br_x = option[8][1]
+				self.scanner_br_x = option[8][1]			
+
 			#Y Axis for scan Area
 			option = self.get_scanner_option('br-y')
 			if option:
@@ -49,30 +60,31 @@ class scanner():
 			
 			
 			#Brightness and Threshold
-			#self.light_parameter_state = False 
-			#options = self.get_scanner_option ('brightness')
-			#if options:
-			#	self.light_parameter_state = True
-			#	self.light_parameter = "brightness"
-			#	try:
-			#		self.min =  options[-1][0]
-			#		self.max = options[-1][1]
-			#	except:
-			#		self.min = -100
-			#		self.max = 100
+			self.light_parameter_state = False 
+			options = self.get_scanner_option ('brightness')
+			if options:
+				self.light_parameter_state = True
+				self.light_parameter = "brightness"
+				try:
+					self.min =  options[-1][0]
+					self.max = options[-1][1]
+				except:
+					self.min = -100
+					self.max = 100
 
-			#options = self.get_scanner_option ('threshold')
-			#if options:
-			#	self.light_parameter_state = True
-			#	self.light_parameter = "threshold"
-			#	try:
-			#		min =  options[-1][0]
-			#		max = options[-1][1]
-			#	except:
-			#		self.min = 0
-			#		self.max = 255
-
+			options = self.get_scanner_option ('threshold')
+			if options:
+				self.light_parameter_state = True
+				self.light_parameter = "threshold"
+				try:
+					self.min =  options[-1][0]
+					self.max = options[-1][1]
+				except:
+					self.min = 0
+					self.max = 255
 			
+			if (self.driver == 1):
+				self.close()
 			
 			
 	
@@ -86,42 +98,76 @@ class scanner():
 
 	def scan(self,file_name,resolution,brightness,region):
 		#Setting Brightness and Threshold
-		#if self.check_brightness_support():
-			#value = (((self.max-self.min)/200)*brightness)-abs(self.min)
-			#print("Min = {0}, Max = {1}, User Value = {2}, Range = {3}, Value = {4}".format(self.min,self.max,brightness,self.max-self.min,value))  
-			#if self.light_parameter == "brightness":
-			#	try:
-			#		self.scanner.brightness = value
-			#	except AttributeError:
-			#		print ("ooh")
-			#if self.light_parameter == "threshold":
-			#		try:
-			#			self.scanner.threshold = value
-			#		except AttributeError:
-		#				print ("Ohhh")
+		if self.check_brightness_support():
+			brightness_value = ((self.max-self.min)/200)*brightness
+			if (self.min < 0):
+				brightness_value = brightness_value - 100
+			print("Scanner Max = {0},  Scanner Min = {1}, User Value (200) = {2}, Corected Value {3}".format(self.max,self.min,brightness,brightness_value))  
+
 		
-		#Setting Resolution
-		self.scanner.resolution = resolution
+		if (self.driver == 0):
+			if (self.check_brightness_support()):
+				if self.light_parameter == "brightness":
+					try:
+						self.scanner.brightness = brightness_value
+					except AttributeError:
+						print ("ooh")
+				if self.light_parameter == "threshold":
+					try:
+						self.scanner.threshold = brightness_value
+					except AttributeError:
+						print ("Ohhh")
+			
+			#Setting Resolution
+			self.scanner.resolution = resolution
+			
+			
+			#Setting Region
+			print("Redion = ",region)
+			if region == 0:
+				self.scanner.br_y = self.br_y_pass
+			elif region == 1:
+				self.scanner.br_y = 3*(self.br_y_pass/4)
+			elif region == 2:
+				self.scanner.br_y = self.br_y_pass/2
+			elif region == 3:
+				self.scanner.br_y = self.br_y_pass/4
+			else:
+				pass
+			print(self.scanner.br_y)	       
 		
-		#Setting Region
-		if region == 0:
-			self.scanner.br_y = self.br_y_pass
-		elif region == 1:
-			self.scanner.br_y = 3*(self.br_y_pass/4) 
-		elif region == 2:
-			self.scanner.br_y = self.br_y_pass/2
-		elif region == 3:
-			self.scanner.br_y = self.br_y_pass/4
+
+			#Scanning	
+			#try:
+			pil_image = self.scanner.scan()
+			#except:
+			#	print("Error")
+			#else:
+			pil_image.save(file_name)
 		else:
-			pass        
-		
-		#Scanning	
-		#try:
-		pil_image = self.scanner.scan()
-		#except:
-		#	print("Error")
-		#else:
-		pil_image.save(file_name)
+			
+			#Setting Region
+			if region == 0:
+				self.scanner_br_y = self.br_y_pass
+			elif region == 1:
+				self.scanner_br_y = 3*(self.br_y_pass/4)
+			elif region == 2:
+				self.scanner_br_y = self.br_y_pass/2
+			elif region == 3:
+				self.scanner_br_y = self.br_y_pass/4
+			else:
+				pass
+			
+			print(self.device[0])
+			if (self.check_brightness_support()):
+				print("scanimage --device-name='{0}' --resolution {1} --mode {2} -x {3} -y {4} --{5}={6} > {7}".format(self.device[0],resolution,self.scanner_mode,self.scanner_br_x,self.scanner_br_y,self.light_parameter,brightness_value,file_name))	
+				os.system("scanimage --device-name='{0}' --resolution {1} --mode {2} -x {3} -y {4} --{5}={6} > {7}".format(self.device[0],resolution,self.scanner_mode,self.scanner_br_x,self.scanner_br_y,self.light_parameter,brightness_value,file_name))	
+			else:
+				print("scanimage --device-name='{0}' --resolution {1} --mode {2} -x {3} -y {4} > {5}".format(self.device[0],resolution,self.scanner_mode,self.scanner_br_x,self.scanner_br_y,file_name))	
+				os.system("scanimage --device-name='{0}' --resolution {1} --mode {2} -x {3} -y {4} > {5}".format(self.device[0],resolution,self.scanner_mode,self.scanner_br_x,self.scanner_br_y,file_name))	
+
+
+
 
 	def close(self):
 		self.scanner.close()
