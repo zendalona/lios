538982e4d0543c54cae250a2f00b335b427592e5
Nalin.x.GNU <Nalin.x.Linux@gmail.com>
2015-11-26 20:26:50 +0530

    more packages added

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(name='lios',
       author_email='Nalin.x.Linux@gmail.com',
       url='https://github.com/Nalin-x-Linux/',
       license = 'GPL-3',
-      packages=['lios'],
+      packages=['lios','lios/ocr','lios/scanner','lios/ui/','lios/ui/gtk'],
       data_files=[('share/lios/',['share/lios/readme','share/lios/lios']),('share/applications/',['share/applications/Lios.desktop']),('bin',['bin/lios'])]
       )
+# sudo python3 setup.py install --install-data=/usr
