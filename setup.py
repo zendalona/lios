e21628390175d2020312e94f9540d42647100829
nalin.x.linux <nalin.x.linux@gmail.com>
2016-12-30 11:40:09 +0530

    Trainer files added and info updated

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 from distutils.core import setup
 from glob import glob
 setup(name='lios',
-      version='2.0',
-      description='Python Distribution Utilities',
+      version='2.5',
+      description='Easy-OCR solution and Tesseract trainer for GNU/Linux',
       author='Nalin.x.Linux',
-      author_email='Nalin.x.Linux@gmail.com',
-      url='https://github.com/Nalin-x-Linux/',
+      author_email='nalin.x.Linux@gmail.com',
+      url='https://gitlab.com/Nalin-x-Linux/lios-3',
       license = 'GPL-3',
       packages=['lios','lios/ocr','lios/scanner','lios/ui/','lios/ui/gtk'],
-      data_files=[('share/lios/',['share/lios/readme.text','share/lios/lios.png']),('share/applications/',['share/applications/Lios.desktop']),('share/man/man1/',['share/man/man1/lios.1.gz']),('share/doc/lios/',['share/doc/lios/copyright']),('share/pixmaps/',['share/pixmaps/lios.xpm']),('bin',['bin/lios'])]
+      data_files=[('share/lios/',['share/lios/readme.text','share/lios/text_cleaner_list.text','share/lios/lios.png']),
+      ('share/applications/',['share/applications/Lios.desktop','share/applications/Tesseract-Trainer.desktop']),
+      ('share/man/man1/',['share/man/man1/lios.1.gz','share/man/man1/train-tesseract.1.gz']),
+      ('share/doc/lios/',['share/doc/lios/copyright']),('share/pixmaps/',['share/pixmaps/lios.xpm']),
+      ('bin',['bin/lios','bin/train-tesseract'])]
       )
 # sudo python3 setup.py install --install-data=/usr
