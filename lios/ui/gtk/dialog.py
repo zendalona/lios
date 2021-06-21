73fc343c7929bb385ded53130d842dc5d084bfe3
Mikhail Rudenko <mike.rudenko@gmail.com>
2021-06-21 21:29:30 +0300

    Use exact versions when importing Gtk and friends
    This is necessary in environments where Gtk-3 and Gtk-4 are both
    installed. Otherwise, multiple `AttributeError`s arise, e.g.
    
    ```
    Traceback (most recent call last):
      File "/usr/bin/lios", line 19, in <module>
        from lios.main import *
      File "/usr/lib/python3.9/site-packages/lios/main.py", line 27, in <module>
        from lios import scanner, editor, imageview, cam, ocr, preferences, speech
      File "/usr/lib/python3.9/site-packages/lios/editor.py", line 20, in <module>
        from lios.ui.gtk import text_view, tree_view, widget, dialog, file_chooser, containers, window
      File "/usr/lib/python3.9/site-packages/lios/ui/gtk/widget.py", line 166, in <module>
        class Separator(Gtk.HSeparator):
      File "/usr/lib/python3.9/site-packages/gi/overrides/__init__.py", line 32, in __getattr__
        return getattr(self._introspection_module, name)
      File "/usr/lib/python3.9/site-packages/gi/module.py", line 123, in __getattr__
        raise AttributeError("%r object has no attribute %r" % (
    AttributeError: 'gi.repository.Gtk' object has no attribute 'HSeparator'
    ```

--- lios/ui/gtk/dialog.py
+++ lios/ui/gtk/dialog.py
@@ -18,6 +18,9 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ###########################################################################
 
+import gi
+gi.require_version("Gtk", "3.0")
+
 from gi.repository import Gtk
 
 
